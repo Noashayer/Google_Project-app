921fe18df558abe6d44858b72a94ab98d71fe2bd
Noashayer <noa.shayer1@gmail.com>
2025-05-03 20:57:32 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,3 +4,5 @@ st.title("🎈 The google app")
 st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
+# Get text input from the user
+user_text = st.text_input("Please enter some text:")
